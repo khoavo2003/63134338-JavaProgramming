b5b239afe07ee849734c608249d0361b1aa62c95
Chi Khoa <111358604+vckhoa1214@users.noreply.github.com>
2024-01-24 19:55:51 +0700

    Ví dụ
    Demo

---
+++ HelloWorld/src/chikhoa/XinChao.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package chikhoa;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+	}
+
+}
