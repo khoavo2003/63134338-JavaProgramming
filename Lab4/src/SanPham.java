f611f6d3da0888d741c1c878e39adc91fceb35a6
Chi Khoa <111358604+vckhoa1214@users.noreply.github.com>
2024-03-05 19:39:57 +0700

    Update SanPham.java

--- Lab4/src/SanPham.java
+++ Lab4/src/SanPham.java
@@ -5,6 +5,40 @@ public class SanPham {
     private double donGia;
     private double giamGia;
 
+    public SanPham(String tenSp, double donGia, double giamGia) {
+        this.tenSp = tenSp;
+        this.donGia = donGia;
+        this.giamGia = giamGia;
+    }
+
+    public SanPham(String tenSp, double donGia) {
+        this(tenSp, donGia, 0);
+    }
+
+    public String getTenSp() {
+        return this.tenSp;
+    }
+
+    public void setTenSp(String tenSp) {
+        this.tenSp = tenSp;
+    }
+
+    public double getDonGia() {
+        return this.donGia;
+    }
+
+    public void setDonGia(double donGia) {
+        this.donGia = donGia;
+    }
+
+    public double getGiamGia() {
+        return this.giamGia;
+    }
+
+    public void setGiamGia(double giamGia) {
+        this.giamGia = giamGia;
+    }
+
     public double getThueNhapKhau() {
         return this.donGia * 0.1;
     }
@@ -14,11 +48,5 @@ public class SanPham {
         System.out.println("Đơn giá: " + this.donGia);
         System.out.println("Giảm giá: " + this.giamGia);
         System.out.println("Thuế nhập khẩu: " + this.getThueNhapKhau());
-    }
-
-    public void nhap(String tenSp, double donGia, double giamGia) {
-        this.tenSp = tenSp;
-        this.donGia = donGia;
-        this.giamGia = giamGia;
     }
 }
